a5cae530fc5b7dc0d557e52cdf2782c7518159b6
Jakub Záruba <eflyax42@gmail.com>
2017-11-07 17:37:07 +0100

    Added missing events
    Added events: AddToWishlist, Search, Lead, CompleteRegistration, InitiateCheckout

--- src/FacebookPixel/FacebookPixel.php
+++ src/FacebookPixel/FacebookPixel.php
@@ -9,12 +9,22 @@ class FacebookPixel extends Control
 
     const EVENT_VIEW_CONTENT = 'ViewContent',
         EVENT_PURCHASE = 'Purchase',
+        EVENT_ADD_TO_WISHLIST = 'AddToWishlist',
+        EVENT_SEARCH = 'Search',
+        EVENT_LEAD = 'Lead',
+        EVENT_COMPLETE_REGISTRATION = 'CompleteRegistration',
+        EVENT_INITIATE_CHECKOUT = 'InitiateCheckout',
         EVENT_ADD_TO_CART = 'AddToCart';
 
     private $sessionEvents = [
         self::EVENT_ADD_TO_CART,
         self::EVENT_VIEW_CONTENT,
         self::EVENT_PURCHASE,
+        self::EVENT_ADD_TO_WISHLIST,
+        self::EVENT_LEAD,
+        self::EVENT_SEARCH,
+        self::EVENT_COMPLETE_REGISTRATION,
+        self::EVENT_INITIATE_CHECKOUT,
     ];
 
     /** @var string */
@@ -53,8 +63,16 @@ class FacebookPixel extends Control
         $this->clearEventsFromSession();
     }
 
+    /**
+     * When a key page is viewed such as a product page, e.g. landing on a product detail page
+     * @param array $contentIds
+     * @param null $contentName
+     * @param null $contentCategory
+     * @param null $value
+     * @param null $currency
+     */
     public function viewContent(
-        $contentIds,
+        $contentIds = [],
         $contentName = null,
         $contentCategory = null,
         $value = null,
@@ -73,8 +91,16 @@ class FacebookPixel extends Control
         );
     }
 
+    /**
+     * When a product is added to the shopping cart, e.g. click on add to cart button
+     * @param array $contentIds
+     * @param null $contentName
+     * @param null $contentCategory
+     * @param null $value
+     * @param null $currency
+     */
     public function addToCart(
-        $contentIds,
+        $contentIds = [],
         $contentName = null,
         $contentCategory = null,
         $value = null,
@@ -87,11 +113,137 @@ class FacebookPixel extends Control
         );
     }
 
+    /**
+     * When a registration form is completed, e.g. complete subscription/signup for a service
+     */
+    public function completeRegistration()
+    {
+        $this->sendEventToOutput(
+            self::EVENT_COMPLETE_REGISTRATION,
+            null
+        );
+    }
+
+    /**
+     * When a purchase is made or checkout flow is completed, e.g. landing on thank you/confirmation page
+     * @param $value
+     * @param $currency
+     * @param null $contentIds
+     */
     public function purchase($value, $currency, $contentIds = null)
     {
         $this->sendEventToOutput(
             self::EVENT_PURCHASE,
             $this->prepareProductsToParameters($contentIds, null, null, $value, $currency)
+        );
+    }
+
+    /**
+     * When a person enters the checkout flow prior to completing the checkout flow, e.g. click on checkout button
+     * @param null $value
+     * @param null $currency
+     * @param null $contentName
+     * @param null $contentCategory
+     * @param null $contentIds
+     * @param null $contents
+     * @param null $numItems
+     */
+    public function initiateCheckout(
+        $value = null,
+        $currency = null,
+        $contentName = null,
+        $contentCategory = null,
+        $contentIds = null,
+        $contents = null,
+        $numItems = null
+    )
+    {
+
+    }
+
+    /**
+     * When a search is made, e.g. when a product search query is made
+     * @param null $value
+     * @param null $currency
+     * @param null $contentCategory
+     * @param null $contentIds
+     * @param null $contents
+     * @param null $searchString
+     */
+    public function search(
+        $value = null,
+        $currency = null,
+        $contentCategory = null,
+        $contentIds = null,
+        $contents = null,
+        $searchString = null
+    )
+    {
+        $this->sendEventToOutput(
+            self::EVENT_SEARCH,
+            $this->prepareProductsToParameters(
+                $contentIds,
+                null,
+                $contentCategory,
+                $value,
+                $currency,
+                $contents,
+                $searchString
+            )
+        );
+    }
+
+    /**
+     * When a sign up is completed, e.g. click on pricing, signup for trial
+     * @param null $value
+     * @param null $currency
+     * @param null $contentName
+     * @param null $contentCategory
+     */
+    public function lead($value = null, $currency = null, $contentName = null, $contentCategory = null)
+    {
+        $this->sendEventToOutput(
+            self::EVENT_SEARCH,
+            $this->prepareProductsToParameters(
+                null,
+                $contentName,
+                $contentCategory,
+                $value,
+                $currency,
+                null,
+                null
+            )
+        );
+    }
+
+    /**
+     * When a product is added to a wishlist, e.g. click on add to wishlist button
+     * @param null $value
+     * @param null $currency
+     * @param null $contentName
+     * @param null $contentCategory
+     * @param array $contentIds
+     * @param null $contents
+     */
+    public function addToWishlist(
+        $value = null,
+        $currency = null,
+        $contentName = null,
+        $contentCategory = null,
+        $contentIds = [],
+        $contents = null
+    )
+    {
+        $this->sendEventToOutput(
+            self::EVENT_ADD_TO_WISHLIST,
+            $this->prepareProductsToParameters(
+                $contentIds,
+                $contentName,
+                $contentCategory,
+                $value,
+                $currency,
+                $contents
+            )
         );
     }
 
@@ -112,6 +264,8 @@ class FacebookPixel extends Control
      * @param string $contentCategory
      * @param float $value
      * @param string $currency
+     * @param string $contents
+     * @param null $searchString
      * @return string
      */
     private function prepareProductsToParameters(
@@ -119,7 +273,10 @@ class FacebookPixel extends Control
         $contentName = null,
         $contentCategory = null,
         $value = null,
-        $currency = null)
+        $currency = null,
+        $contents = null,
+        $searchString = null
+    )
     {
         if (!is_array($contentIds)) {
             $contentIds = [$contentIds];
@@ -135,6 +292,8 @@ class FacebookPixel extends Control
         $parameters['content_category'] = $contentCategory ? $contentCategory : null;
         $parameters['value'] = $value ? $this->formatPrice($value) : null;
         $parameters['currency'] = $currency ?: null;
+        $parameters['contents'] = $contents ?: null;
+        $parameters['search_string'] = $searchString ?: null;
 
         foreach ($parameters as $key => $value) {
             if (!$value) {
